352fa3b7d0605200eab54ed241e0c79e37145ec9
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-09 16:08:29 -0400

    add order method to controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,16 @@
 class ReportsController < ApplicationController
   def all_data
     @assembly = Assembly.find_by_name(params[:name] || "a1")
-    @hits = @assembly.hits
-    # @assembly.sequences.each do |s|
-    #   s.genes.each do |g|
-    #     g.hits.each do |h|
-    #       @hits << h
-    #     end
-    #   end
-    # end
-    # @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
+    @hits = @assembly.hits.order("percent_similarity DESC")
   end
 end
+
+
+# @assembly.sequences.each do |s|
+#   s.genes.each do |g|
+#     g.hits.each do |h|
+#       @hits << h
+#     end
+#   end
+# end
+# @hits.sort! {|a, b| b.percent_similarity <=> a.percent_similarity}
