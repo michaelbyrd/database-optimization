26b86258f89c4452dcbb7dcd392e07dd6e36bf20
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-03-09 15:42:21 -0400

    update db associations, add index

---
+++ db/migrate/20150309181117_add_indices.rb
@@ -0,0 +1,8 @@
+class AddIndices < ActiveRecord::Migration
+  def change
+    add_index :assemblies, :name
+    add_index :sequences, :assembly_id
+    add_index :genes, :sequence_id
+    add_index :hits, :subject_id
+  end
+end
